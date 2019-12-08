9f760a443d63da7182c2482b612efaf72a88ac2f
chibimami6 <chibimami6@gmail.com>
2019-12-08 20:03:17 +0900

    jQuery追加

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
-$(function () {
+// $(function () {})
 
-
-
-
-    
+$(".footer-right").on("click", function () {
+    // console.log(123);
+    $("html, body").animate({ scrollTop: 0 }, 1500);
 })
+
+
